8a39b9b64784bf77be495654e3026ccf513d20b9
Manuel <manuelsantiengineering@gmail.com>
2019-04-30 14:11:48 -0400

    Included the Annotations for security and the boostract and auth-service ymls

--- auth-service/src/main/java/com/msanti/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/msanti/authservice/AuthServiceApplication.java
@@ -2,7 +2,16 @@ package com.msanti.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+// Enables the discovery client implementation to let our auth service register in Registry Service
+@EnableDiscoveryClient
+// Enables a Spring Security filter that authenticates requests via an incoming OAuth2 token
+@EnableResourceServer
+// Enables Spring Security global method security
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class AuthServiceApplication {
 
