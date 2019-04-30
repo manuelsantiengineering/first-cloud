05ef3274369466c15dd3b0fd65f0e27b4ef051eb
Manuel <manuelsantiengineering@gmail.com>
2019-04-30 14:22:01 -0400

    Included the enums and the User implementation of UserDetails

---
+++ auth-service/src/main/java/com/msanti/authservice/enums/Authorities.java
@@ -0,0 +1,15 @@
+package com.msanti.authservice.enums;
+
+import org.springframework.security.core.GrantedAuthority;
+
+// This enum is responsible to define the authorities of our auth service and it 
+// implements the GrantedAuthority interface which represents an authority 
+// granted to an Authentication object.
+public enum Authorities implements GrantedAuthority {
+  ROLE_USER;
+
+  @Override
+  public String getAuthority() {
+      return name();
+  }
+}
