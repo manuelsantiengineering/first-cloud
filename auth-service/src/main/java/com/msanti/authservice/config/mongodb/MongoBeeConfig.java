5b56bdcebd06765440661b1bcf1993b517a19e6d
Manuel <manuelsantiengineering@gmail.com>
2019-04-30 15:03:20 -0400

    Cleaned up code

--- auth-service/src/main/java/com/msanti/authservice/config/mongodb/MongoBeeConfig.java
+++ auth-service/src/main/java/com/msanti/authservice/config/mongodb/MongoBeeConfig.java
@@ -12,27 +12,27 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @DependsOn("mongoTemplate")
 public class MongoBeeConfig {
 
-    private static final String MONGODB_URL_FORMAT = "mongodb://%s:%s@%s:%d/%s";
-    private static final String MONGODB_CHANGELOGS_PACKAGE = "com.marcusdacoregio.authservice.config.mongodb.changelogs";
-
-    @Autowired
-    private MongoProperties mongoProperties;
-
-    @Autowired
-    private MongoTemplate mongoTemplate;
-
-    @Bean
-    public Mongobee mongobee() {
-        Mongobee runner = new Mongobee(String.format(MONGODB_URL_FORMAT,
-                mongoProperties.getUsername(),
-                mongoProperties.getPassword(),
-                mongoProperties.getHost(),
-                mongoProperties.getPort(),
-                mongoProperties.getDatabase()));
-        runner.setMongoTemplate(mongoTemplate);
-        runner.setDbName(mongoProperties.getDatabase());
-        runner.setChangeLogsScanPackage(MONGODB_CHANGELOGS_PACKAGE);
-        return runner;
-    }
+  private static final String MONGODB_URL_FORMAT = "mongodb://%s:%s@%s:%d/%s";
+  private static final String MONGODB_CHANGELOGS_PACKAGE = "com.msanti.authservice.config.mongodb.changelogs";
+  
+  @Autowired
+  private MongoProperties mongoProperties;
+  
+  @Autowired
+  private MongoTemplate mongoTemplate;
+  
+  @Bean
+  public Mongobee mongobee() {
+      Mongobee runner = new Mongobee(String.format(MONGODB_URL_FORMAT,
+              mongoProperties.getUsername(),
+              mongoProperties.getPassword(),
+              mongoProperties.getHost(),
+              mongoProperties.getPort(),
+              mongoProperties.getDatabase()));
+      runner.setMongoTemplate(mongoTemplate);
+      runner.setDbName(mongoProperties.getDatabase());
+      runner.setChangeLogsScanPackage(MONGODB_CHANGELOGS_PACKAGE);
+      return runner;
+  }
 
 }
