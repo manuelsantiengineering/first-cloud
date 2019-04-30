5d9ecf457816574c8dce772a58966492eafca355
Manuel <manuelsantiengineering@gmail.com>
2019-04-30 14:43:10 -0400

    Included the service annotation to the Custom User Details Service and included the Authentication Client Details Service

--- auth-service/src/main/java/com/msanti/authservice/service/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/msanti/authservice/service/CustomUserDetailsService.java
@@ -3,9 +3,11 @@ package com.msanti.authservice.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.msanti.authservice.repository.UserRepository;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
   
   private final UserRepository userRepository;
