45eb23d7ff98013ce91b427e30ba5ca81f22ff97
Manuel <manuelsantiengineering@gmail.com>
2019-04-30 13:56:15 -0400

    Fixed the default gateway service configuratiion

--- gateway-service/src/main/java/com/msanti/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/msanti/gatewayservice/GatewayServiceApplication.java
@@ -2,7 +2,13 @@ package com.msanti.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+// Tells Spring to enable the discovery client implementation.
+@EnableDiscoveryClient
+// Sets up a Zuul Server so it can forward requests to other services
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayServiceApplication {
 
